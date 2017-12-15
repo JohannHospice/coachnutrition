98c7681f6966e19b74b5ede36b6a57b97c54e5bb
Johann Hospice <johannhospice.dev@gmail.com>
2017-12-15 14:36:23 +0100

    Blabla modif

--- app/src/main/java/fr/univ_paris_diderot/coachnutrition/MainActivity.java
+++ app/src/main/java/fr/univ_paris_diderot/coachnutrition/MainActivity.java
@@ -1,13 +1,37 @@
 package fr.univ_paris_diderot.coachnutrition;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Currency;
+import java.util.Date;
+
+import fr.univ_paris_diderot.coachnutrition.app.database.NutritionResolverHandler;
 
 public class MainActivity extends AppCompatActivity {
+    private NutritionResolverHandler resolverHandler;
+    private TextView minCalorie;
+    private TextView maxCalorie;
+    private TextView currentCalorie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resolverHandler = new NutritionResolverHandler(getApplicationContext());
+
+        Date today = new Date();
+        Cursor cursor = resolverHandler.getDay(today);
+
+        if (cursor.getCount() != 0) {
+
+        }
+        minCalorie = findViewById(R.id.min_calorie);
+        maxCalorie = findViewById(R.id.max_calorie);
+        currentCalorie = findViewById(R.id.current_calorie);
     }
+
 }
--- app/src/main/java/fr/univ_paris_diderot/coachnutrition/app/database/NutritionResolverHandler.java
+++ app/src/main/java/fr/univ_paris_diderot/coachnutrition/app/database/NutritionResolverHandler.java
@@ -7,18 +7,26 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 
+import java.util.Date;
+
 public class NutritionResolverHandler {
     private ContentResolver resolver;
 
-    NutritionResolverHandler(Context context) {
+    public NutritionResolverHandler(Context context) {
         resolver = context.getContentResolver();
     }
 
-    public Cursor query(String table, long id) {
+    public Cursor getId(String table, long id) {
         Uri.Builder builder = new Uri.Builder();
         builder.scheme("content").authority(NutritionProvider.AUTHORITY).appendPath(table);
         Uri uri = ContentUris.appendId(builder, id).build();
-        return  resolver.query(uri, null, Contract.Statistic._ID + " = ?", new String[]{String.valueOf(id)}, null);
+        return resolver.query(uri, null, Contract.Statistic._ID + " = ?", new String[]{String.valueOf(id)}, null);
+    }
+
+    public Cursor query(String table, String[] projection, String selection, String[] selectionArgs) {
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("content").authority(NutritionProvider.AUTHORITY).appendPath(table);
+        return resolver.query(builder.build(), projection, selection, selectionArgs, null);
     }
 
     public Uri insert(String table, ContentValues contentValues) {
@@ -30,12 +38,16 @@ public class NutritionResolverHandler {
     }
 
     public int delete(String where, String[] selectionArgs) {
-        return resolver.delete(buildUri(Contract.Food.TABLE_NAME), where,  selectionArgs);
+        return resolver.delete(buildUri(Contract.Food.TABLE_NAME), where, selectionArgs);
     }
 
     private static Uri buildUri(String table) {
         Uri.Builder builder = new Uri.Builder();
         builder.scheme("content").authority(NutritionProvider.AUTHORITY).appendPath(table);
         return builder.build();
+    }
+
+    public Cursor getDay(Date today) {
+        return query(Contract.Day.TABLE_NAME, null, "date = ?", new String[]{String.valueOf(today.getTime())});
     }
 }
